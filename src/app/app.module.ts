7848f2d07acf3607325bb57a270a001023d25e30
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-10-26 03:02:07 +0800

    create all of components and create the routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { UsersComponent } from './admin/users/users.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    UsersComponent,
+    RoomsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
