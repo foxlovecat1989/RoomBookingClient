a885689931c9eb44185c26e0bf0ebcce9ac6bbd3
foxlovecat1989 <foxlovecat1989@gmail.com>
2021-10-27 19:53:32 +0800

    implements room component

---
+++ src/app/data.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Layout, LayoutCapacity, Room } from 'src/Model/Room';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private rooms = new Array<Room>();
+
+  constructor() {
+
+    this.generateDummyRooms();
+
+  }
+
+  private generateDummyRooms() {
+    this.rooms = new Array<Room>();
+    const room1 = new Room();
+    room1.id = 1;
+    room1.name = 'First Room';
+    room1.location = 'First Floor';
+    const capacity1 = new LayoutCapacity();
+    capacity1.layout = Layout.THEATER;
+    capacity1.capacity = 50;
+    const capacity2 = new LayoutCapacity();
+    capacity2.layout = Layout.USHAPE;
+    capacity2.capacity = 20;
+    room1.layoutCapacities.push(capacity1);
+    room1.layoutCapacities.push(capacity2);
+
+    const room2 = new Room();
+    room2.id = 2;
+    room2.name = 'Second Room';
+    room2.location = 'Third Floor';
+    const capacity3 = new LayoutCapacity();
+    capacity3.layout = Layout.THEATER;
+    capacity3.capacity = 60;
+    room2.layoutCapacities.push(capacity3);
+
+    this.rooms.push(room1);
+    this.rooms.push(room2);
+  }
+
+  getRooms() : Array<Room>{
+    return this.rooms;
+  }
+}
